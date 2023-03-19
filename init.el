29656697a87a9e8d305dc03d915108bddf1b117c
Zoltán Király <zoliky@gmail.com>
2023-03-19 15:07:02 +0200

    Initial commit

---
+++ init.el
@@ -0,0 +1,30 @@
+;;; init.el --- Initialization file -*- lexical-binding: t; -*-
+
+;; Copyright (c) 2023 Zoltán Király
+;; Created: March 19, 2023
+
+;; Author: Zoltán Király <zoliky@gmail.com>
+
+;; This file is NOT part of GNU Emacs.
+
+;; This file is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the
+;; Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This file is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This file specifies how to initialize Emacs
+;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Init-File.html
+
+;;; Code:
+
+;;; init.el ends here
