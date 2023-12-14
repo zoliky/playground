d26d339e7ef4f1f564a402cdb842f89dd7ff933b
Zoltán Király <zoliky@gmail.com>
2023-12-14 21:25:31 +0200

    Sync

--- early-init.el
+++ early-init.el
@@ -1,24 +1,23 @@
 ;;; early-init.el --- Early init file -*- lexical-binding: t; -*-
 
-;; Copyright (c) 2020-2023 Zoltán Király
+;; Copyright (C) 2020-2023 Zoltán Király
 
-;; Author: Zoltán Király <zoliky@gmail.com>
-;; Created: March 19, 2023
+;; Author: Zoltán Király <zoltan.public@fastmail.com>
 
 ;; This file is NOT part of GNU Emacs.
 
-;; This file is free software: you can redistribute it and/or modify it
-;; under the terms of the GNU General Public License as published by the
-;; Free Software Foundation, either version 3 of the License, or (at
-;; your option) any later version.
-;;
-;; This file is distributed in the hope that it will be useful, but
-;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; This file is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published
+;; by the Free Software Foundation, either version 3 of the License,
+;; or (at your option) any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 ;; General Public License for more details.
-;;
+
 ;; You should have received a copy of the GNU General Public License
-;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
+;; along with this file. If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
