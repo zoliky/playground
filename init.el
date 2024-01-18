7ac063cecbe17f7dfb6726bfe8f175bf75872c2b
Zoltán Király <3882981+zoliky@users.noreply.github.com>
2024-01-18 18:23:14 +0200

    Sync

--- init.el
+++ init.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2020-2024 Zoltán Király
 
-;; Author: Zoltán Király <zoltanpublic@fastmail.com>
+;; Author: Zoltán Király <email@email.com>
 
 ;; This file is NOT part of GNU Emacs.
 
