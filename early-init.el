d3afb52f7b648c0ddb87c6bc1c5a72613c9c5738
Zoltán Király <3882981+zoliky@users.noreply.github.com>
2025-07-06 10:36:42 +0300

    Sync

--- early-init.el
+++ early-init.el
@@ -39,7 +39,7 @@
 ;; Maximize the Emacs frame on startup
 (push '(fullscreen . maximized) default-frame-alist)
 
-;; Set initial frame size to 1920x1080 text pixels
+;; Set initial frame size to 1920x1080 in text pixels
 (push '(width  . (text-pixels . 1920)) default-frame-alist)
 (push '(height . (text-pixels . 1080)) default-frame-alist)
 
