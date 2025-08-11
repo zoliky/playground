6b77bb2e93c2dd58eb714c9f8446b9cce4730f5a
Zoltán Király <3882981+zoliky@users.noreply.github.com>
2025-08-11 12:49:05 +0300

    Sync

--- early-init.el
+++ early-init.el
@@ -49,7 +49,7 @@
 ;; Customize titlebar to show buffer name without hostname
 (setq frame-title-format '(multiple-frames "%b" ("" "%b - GNU Emacs")))
 
-;; Disable the menu bar, tool bar, and scroll bars
+;; Disable GUI elements to keep the interface minimal
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
